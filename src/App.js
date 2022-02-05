d135188c7121fa0c221d6b6b3e91087a9b2d5fa6
Robin Ghyselinck <rghyselinck@gmail.com>
2022-02-05 20:43:20 +0100

    imlpemented custom hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+const App = (props) => {
+    const left = useCounter()
+    const right = useCounter()
+    const name = useField('text')
+    const born = useField('date')
+    const height = useField('number')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div>
+        <div>
+            {left.value}
+            <button onClick={() => {left.increase()}}>
+            left +
+            </button>
+            <button onClick={() => {right.increase()}}>
+            right +
+            </button>
+            {right.value}
+        </div>
+
+        <div>
+          <form>
+              name:
+              <input  {...name} />
+              <br/>
+              birthdate:
+              <input {...born} />
+              <br />
+              height:
+              <input {...height} />
+          </form>
+          <div>
+              {name.value} {born.value} {height.value}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+const useCounter = () => {
+  const [value, setValue] = useState(0)
+
+    const increase = () => {
+      setValue(value + 1)
+    }
+
+    const decrease = () => {
+      setValue(value - 1)
+    }
+
+    const zero = () => {
+      setValue(0)
+    }
+
+    return { value, increase, decrease, zero }
+}
+
+const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    return {
+        type,
+        value,
+        onChange
+    }
 }
 
 export default App;
